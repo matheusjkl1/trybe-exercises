50bd0dbb05be3f961475cbbb1c62f77ae1eced38
Matheus Mendes <matheus.unilog@gmail.com>
2020-11-26 17:30:09 -0300

    Add exercises 4.2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Trybe Exercises
-Fique a vontade para dar Feedback sobre meus projetos
 
 Aqui voce vai encontrar meus projetos feitos na Trybe.
+
+Fique a vontade para dar Feedback sobre meus projetos.
---
+++ exercises/4/4.1/exercises1.js
@@ -0,0 +1,63 @@
+//Adição
+let a = 5
+let b = 2
+    console.log(a + b);
+//Subtração
+let c = 8
+let d = 1
+    console.log(c - d);
+//Multiplicação
+let e = 2
+let f = 2
+    console.log(a * b);
+//Divisão
+let g = 1
+let h = 6
+    console.log(a / b);
+//Módulo
+let i = 1
+let j = 1
+    console.log(a % b);
+/*Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.*/
+ let Y = 1;
+ let Z = 4;
+
+    if (Y => Z){
+        console.log("Y É Maior.");
+    }
+    else if (Y <= Z){
+        console.log("Z É Maior.");
+    }
+    else {
+        Z * 2
+    };
+/*Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.*/
+    
+    let L = 20;
+    let M = 66;
+    let K = 70;
+   
+       if (L > M && L > K){
+           console.log(L +" Portanto L é maior");
+       }
+       else if (M > L && M > K){
+           console.log(M +" Portanto M é maior");
+       }
+       else {
+           console.log(K +" Portanto K é maior")
+       };
+   
+/*Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.*/
+
+let valor = -1
+
+ if (valor > 0){
+
+    console.log("positive")
+ }
+
+ else if (valor < 0) {
+     console.log("negative")
+ }
+
+ else { console.log("zero")}
