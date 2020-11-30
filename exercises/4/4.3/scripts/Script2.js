4bfa6a801c1bec3bf0e605bffdbbd1af7ea07ea6
Matheus Mendes <matheus.unilog@gmail.com>
2020-11-30 12:02:47 -0300

    Add Directory Scripts

---
+++ exercises/4/4.3/scripts/Script2.js
@@ -0,0 +1,10 @@
+const valorBase = 5;
+let linha = [];
+let asterisco = "*";
+let quebraLinha = "\n";
+let espaco = " ";
+
+for (let indice = 1; indice <= valorBase; indice += 1) {
+  linha += asterisco;
+  console.log(linha);
+}
